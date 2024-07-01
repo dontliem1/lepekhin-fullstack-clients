84047927bcd9fb86e6d233cf5ccca3c2f819c8e7
Mher <shahinyanm@gmail.com>
2024-07-01 10:56:19 +0400

    Did feature for functionality of search and sorting

--- plugins/lepekhin/clients/components/ClientList.php
+++ plugins/lepekhin/clients/components/ClientList.php
@@ -14,7 +14,7 @@ class ClientList extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'        => 'ClientList Component',
+            'name' => 'ClientList Component',
             'description' => 'Viewing, filtering and sorting clients'
         ];
     }
@@ -23,11 +23,16 @@ class ClientList extends ComponentBase
     {
         $this->page['get'] = get();
         $search = get('search');
+        $sort = get('sort');
+        $inverse = get('inverse');
         $this->page['clients'] = Client::when($search, function (Builder $query, $search) {
             return $query->where(function (Builder $query) use ($search) {
                 $searchString = "$search%";
                 return $query->where('name', 'like', $searchString);
             });
-        })->inRandomOrder(/* TODO: sort clients https://wintercms.com/docs/v1.2/docs/database/query#ordering-grouping-limit--offset */)->get();
+        })->when($sort, function(Builder $q) use ($sort, $inverse) {
+            $q->orderBy($sort, $inverse ? 'desc' : 'asc');
+        })
+            ->get();
     }
 }
